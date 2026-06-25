56378e915b14ba5c0d9c57fad93df61f507e6391
Joshua Rodli <joshuarodli19@gmail.com>
2026-06-25 12:16:16 -0700

    create decode function and run go fmt

--- internal/packet/packet.go
+++ internal/packet/packet.go
@@ -1,6 +1,9 @@
 package packet
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+	"fmt"
+)
 
 // Wire format (big-endian):
 //	byte 0: Flag
@@ -8,10 +11,10 @@ import "encoding/binary"
 //	bytes 3-4: Checksum (uint16)
 //	bytes 5+: Payload
 type Packet struct {
-	Flag byte
-	Seq uint16
+	Flag     byte
+	Seq      uint16
 	Checksum uint16
-	Payload []byte
+	Payload  []byte
 }
 
 func (packet *Packet) Encode() []byte {
@@ -21,4 +24,18 @@ func (packet *Packet) Encode() []byte {
 	binary.BigEndian.PutUint16(buf[3:5], packet.Checksum)
 	copy(buf[5:], packet.Payload)
 	return buf
+}
+
+func Decode(buf []byte) (Packet, error) {
+	if len(buf) < 5 {
+		return Packet{}, fmt.Errorf("packet too short: %d bytes", len(buf))
+	}
+	payload := make([]byte, len(buf)-5)
+	copy(payload, buf[5:])
+	return Packet{
+		Flag:     buf[0],
+		Seq:      binary.BigEndian.Uint16(buf[1:3]),
+		Checksum: binary.BigEndian.Uint16(buf[3:5]),
+		Payload:  payload,
+	}, nil
 }
